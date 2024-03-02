3153be03234e2a2d0d3f891b3c3d17f43dbe8b98
Ammar Ahmed <hafiz.ammar33@gmail.com>
2024-03-02 04:34:25 +0100

    Items: Implement CRUD operations in Nest.js

---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+  monogURI:
+    'mongodb+srv://ammar081:120076@nestrestapi.gxz6cmf.mongodb.net/?retryWrites=true&w=majority&appName=nestRestApi',
+};
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,16 +1,42 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
-import { Request, Response } from 'express';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
   @Get()
-  findAll(@Req() req: Request, @Res() res: Response): Response {
-    return res.send('Hello World');
+  findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id): Promise<Item> {
+    return this.itemsService.findOne(id);
   }
 
   @Post()
-  create(@Body() createItemDto: CreateItemDto): string {
-    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  create(@Body() createItemDto: CreateItemDto): Promise<Item> {
+    return this.itemsService.create(createItemDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): Promise<Item> {
+    return this.itemsService.delete(id);
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): Promise<Item> {
+    return this.itemsService.update(id, updateItemDto);
   }
 }
